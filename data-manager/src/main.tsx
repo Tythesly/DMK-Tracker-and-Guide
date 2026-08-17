5bf68b5b24a2f3c85693e4576eb392ea0d27d02a
Tythesly <2800945+Tythesly@users.noreply.github.com>
2026-08-17 16:31:47 -0400

    Add back to top navigation

--- data-manager/src/main.tsx
+++ data-manager/src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import App from "./App";
+import BackToTopButton from "./components/BackToTopButton";
+
+ReactDOM.createRoot(
+  document.getElementById(
+    "root",
+  ) as HTMLElement,
+).render(
   <React.StrictMode>
     <App />
+    <BackToTopButton />
   </React.StrictMode>,
 );
